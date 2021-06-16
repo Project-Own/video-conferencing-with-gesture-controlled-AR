8c08158cefab183bc9575efb68e13504c2c20888
Prajapati, Sahas <b10prajapati@gmail.com>
2021-06-16 06:13:58 +0545

    test

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ export const io = new Server(httpServer, {
   },
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Running");
