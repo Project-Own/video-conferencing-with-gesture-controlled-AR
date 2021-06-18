761bb6247c91ee8b32c5fdf766563b9a9e62ff3b
flyingDutchman01 <nirjalprajapati@gmail.com>
2021-06-18 22:27:56 +0545

    change in server

--- src/socket/connection.ts
+++ src/socket/connection.ts
@@ -1,65 +1,32 @@
 import { Server } from "socket.io";
 
 export enum SocketEvent {
-  created = "created",
-  joined = "joined",
-  join = "join",
-  full = "full",
-  ready = "ready",
-  candidate = "candidate",
-  offer = "offer",
-  answer = "answer",
+  me = "me",
+  callEnded = "callEnded",
+  callUser = "callUser",
+  callAccepted = "callAccepted",
+  disconnect="disconnect",
+  connect="connect"
 }
 
+interface CallUserProps {
+  userToCall:string, signalData:string, from:string, name:string
+}
 export const IOConnection = (io: Server) => {
-  io.on("connection", (socket) => {
-    console.log("User Connected :" + socket.id);
 
-    socket.on(SocketEvent.join, function (roomName) {
-      let rooms = io.sockets.adapter.rooms;
-      let room = rooms.get(roomName);
-
-      console.log(`Room ${roomName} Size: ${room?.size}`);
-      //room == undefined when no such room exists.
-      if (room == undefined) {
-        console.log(SocketEvent.created);
-        socket.join(roomName);
-        socket.emit(SocketEvent.created);
-      } else if (room.size === 1) {
-        //room.size == 1 when one person is inside the room.
-        console.log(SocketEvent.joined);
-
-        socket.join(roomName);
-        socket.emit(SocketEvent.joined);
-      } else {
-        //when there are already two people inside the room.
-        console.log(SocketEvent.full);
-
-        socket.emit(SocketEvent.full);
-      }
-      console.log(rooms);
+  io.on(SocketEvent.connect, (socket) => {
+    socket.emit("me", socket.id);
+  
+    socket.on(SocketEvent.disconnect, () => {
+      socket.broadcast.emit(SocketEvent.callEnded)
     });
-
-    //Triggered when the person who joined the room is ready to communicate.
-    socket.on(SocketEvent.ready, function (roomName) {
-      socket.broadcast.to(roomName).emit("ready"); //Informs the other peer in the room.
+  
+    socket.on(SocketEvent.callUser, ({ userToCall, signalData, from, name }:CallUserProps) => {
+      io.to(userToCall).emit(SocketEvent.callUser, { signal: signalData, from, name });
     });
-
-    //Triggered when server gets an icecandidate from a peer in the room.
-
-    socket.on(SocketEvent.candidate, function (candidate, roomName) {
-      console.log(candidate);
-      socket.broadcast.to(roomName).emit("candidate", candidate);
-    });
-
-    socket.on(SocketEvent.offer, function (offer, roomName) {
-      socket.broadcast.to(roomName).emit("offer", offer); //Sends Offer to the other peer in the room.
-    });
-
-    //Triggered when server gets an answer from a peer in the room.
-
-    socket.on(SocketEvent.answer, function (answer, roomName) {
-      socket.broadcast.to(roomName).emit("answer", answer); //Sends Answer to the other peer in the room.
+  
+    socket.on(SocketEvent.callAccepted, (data:any) => {
+      io.to(data.to).emit(SocketEvent.callAccepted, data.signal)
     });
   });
 };
